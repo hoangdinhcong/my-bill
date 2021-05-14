d077df7c3d3e45a4aee77f980f5081c53a0fb798
hoangdinhcong <hodico.iam@gmail.com>
2021-05-14 17:50:06 +0700

    update: admin nav interaction

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -9,7 +9,17 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class AdminComponent {
 
+  isShowMenu = false;
+  isShowProfileMenu = false;
+
   constructor(public authService: AuthService, private router: Router) { }
 
+  toggleMenu(): void {
+    this.isShowMenu = !this.isShowMenu;
+  }
+
+  toggleProfileMenu(): void {
+    this.isShowProfileMenu = !this.isShowProfileMenu;
+  }
 
 }
